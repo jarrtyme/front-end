34d23a74c4254c40f11a2fd8ddc000470d98343d
jarrtyme <jarrtyme@gmail.com>
2025-11-14 02:02:04 +0800

    2.1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,16 +7,16 @@
       style="height: var(--el-header-height-1); background-color: rgba(255, 255, 255, 0.8)"
     ></div>
     <div class="home">
-      <Carousel :items="items" />
+      <Carousel :items="items.items1" />
       <VideoPlayer
         src="https://www.apple.com/105/media/us/macbook-pro/2025/785e1bc4-d1bd-4cf4-b1b3-94b9411c9e74/anim/performance-chip-background/medium.mp4"
         text="MacBook Pro"
       />
 
-      <ImageCarousel :items="items" />
+      <ImageCarousel :items="items.items2" />
 
       <ScrollSnapCarousel
-        :items="items"
+        :items="items.items3"
         :height="400"
         :itemWidth="`calc(min(max(87.5vw, 280px) - 20px, 420px))`"
         :gap="20"
@@ -25,9 +25,9 @@
         snapAlign="start"
       />
 
-      <Desccard :items="items" />
-      <Bigner :items="items" />
-      <SeamlessCarousel :items="items" />
+      <Desccard :items="items.items4" />
+      <Bigner :items="items.items5" />
+      <SeamlessCarousel :items="items.items6" />
     </div>
   </el-main>
 
@@ -44,39 +44,62 @@ import Desccard from '@/views/Home/components/Desccard.vue'
 import Bigner from '@/views/Home/components/Bigner.vue'
 import Header from '@/views/Home/components/Header.vue'
 import Footer from '@/views/Home/components/Footer.vue'
+import { getFileList } from '@/api/upload'
 
-const items = [
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  },
-  {
-    url: 'http://localhost:3000/uploads/images/å¾®ä¿¡å¾ç_20251111174251_10_255_1763054370415_3405086.jpg'
-  }
-]
+const items = ref({
+  items1: [],
+  items2: [],
+  items3: [],
+  items4: [],
+  items5: [],
+  items6: []
+})
+
+const getFileListAll = async () => {
+  const [res1, res2, res3, res4, res5, res6] = await Promise.all([
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items1'
+    }),
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items2'
+    }),
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items3'
+    }),
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items4'
+    }),
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items5'
+    }),
+    getFileList({
+      page: 1,
+      limit: 10,
+      description: 'items6'
+    })
+  ])
+
+  items.value.items1 = res1.data.files
+  items.value.items2 = res2.data.files
+  items.value.items3 = res3.data.files
+  items.value.items4 = res4.data.files
+  items.value.items5 = res5.data.files
+  items.value.items6 = res6.data.files
+}
+
+onMounted(() => {
+  getFileListAll()
+})
 </script>
 
 <style lang="scss" scoped>
